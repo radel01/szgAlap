8a1ab02d217a971b981e35a863a4fd058dc4e9d8
Roszkopf Adel <133266580+radel01@users.noreply.github.com>
2024-03-08 10:15:04 +0100

    művjelek event

--- main.js
+++ main.js
@@ -14,14 +14,24 @@ GOMBSECTION[0].innerHTML = gombOsszeallit();
 let gombELEMEK=document.querySelectorAll(".szamok button")
 
 for (let index = 0; index < gombELEMEK.length; index++) {
-    gombELEMEK[index].addEventListener("click",gombKattintas);    
+    gombELEMEK[index].addEventListener("click",gombKattintasSzam);    
 }
 const KIFEJEZESELEM = document.getElementsByClassName("kifejezes")
 
-function gombKattintas(event){
+function gombKattintasSzam(event){
     console.log(event.target)//az az elem ami kiváltja az eseményt
     for (let index = 0; index < 10; index++) {
         KIFEJEZESELEM[index].innerHTML+=event.target.innerHTML
     }
     
+}
+//művjelek
+let gombELEMEKMuv=document.querySelectorAll(".muvjelek button")
+for (let index = 0; index < gombELEMEKMuv.length; index++) {
+  gombELEMEKMuv[index].addEventListener("click",gombKattintasMuv);    
+}
+
+function gombKattintasMuv(event){
+  KIFEJEZESELEM[0].innerHTML+=event.target.innerHTML
+  
 }
